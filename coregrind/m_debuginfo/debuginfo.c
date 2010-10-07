lihebi/valgrind

commit 2988b842e7a3232f179b5b714808ce0fdf395e97

When opening an mmaped file to see if it's an ELF file that we should read debuginfo from, use VKI_O_LARGEFILE, so as to ensure the open succeeds for large files on 32-bit systems.  Fixes #234064.
git-svn-id: svn://svn.valgrind.org/valgrind/trunk@11397 a5019735-40e9-0310-863c-91ae7b9d1cf9

coregrind/m_debuginfo/debuginfo.c
@@ -727,7 +727,7 @@ ULong VG_(di_notify_mmap)( Addr a, Bool allow_SkFileV )
    /* Peer at the first few bytes of the file, to see if it is an ELF */
    /* object file. Ignore the file if we do not have read permission. */
    VG_(memset)(buf1k, 0, sizeof(buf1k));
-   fd = VG_(open)( filename, VKI_O_RDONLY, 0 );
+   fd = VG_(open)( filename, VKI_O_RDONLY|VKI_O_LARGEFILE, 0 );
    if (sr_isError(fd)) {
       if (sr_Err(fd) != VKI_EACCES) {
          DebugInfo fake_di;

commit 291e750f828f4332878bc49a4676d90891998964

Only use VKI_O_LARGEFILE on platforms where it exists.  This unbreaks the build breakage on Darwin introduced in r11397, which was a fix for #234064.  The breakage was subsequently reported in #253420 and #253452, which this commit fixes.

coregrind/m_debuginfo/debuginfo.c
@@ -606,7 +606,7 @@ ULong VG_(di_notify_mmap)( Addr a, Bool allow_SkFileV )
    DebugInfo* di;
    ULong      di_handle;
    SysRes     fd;
-   Int        nread;
+   Int        nread, oflags;
    HChar      buf1k[1024];
    Bool       debug = False;
    SysRes     statres;
@@ -727,7 +727,11 @@ ULong VG_(di_notify_mmap)( Addr a, Bool allow_SkFileV )
    /* Peer at the first few bytes of the file, to see if it is an ELF */
    /* object file. Ignore the file if we do not have read permission. */
    VG_(memset)(buf1k, 0, sizeof(buf1k));
-   fd = VG_(open)( filename, VKI_O_RDONLY|VKI_O_LARGEFILE, 0 );
+   oflags = VKI_O_RDONLY;
+#  if defined(VKI_O_LARGEFILE)
+   oflags |= VKI_O_LARGEFILE;
+#  endif
+   fd = VG_(open)( filename, oflags, 0 );
    if (sr_isError(fd)) {
       if (sr_Err(fd) != VKI_EACCES) {
          DebugInfo fake_di;